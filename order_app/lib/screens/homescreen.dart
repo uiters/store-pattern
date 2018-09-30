02a792631c9c2744d2d57eb0a46dd47be2db89da
ndc07 <ndc07.it@gmail.com>
2018-09-30 23:47:23 +0700

    homescreen: design list tables

--- order_app/lib/screens/homescreen.dart
+++ order_app/lib/screens/homescreen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import './../UI/theme.dart';
 
 class HomeScreen extends StatefulWidget {
   @override
@@ -8,6 +9,55 @@ class HomeScreen extends StatefulWidget {
 class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Container(
+        decoration: new BoxDecoration(
+            borderRadius: BorderRadius.circular(5.0),
+            border: Border.all(color: fontColorLight.withOpacity(0.2))),
+        margin: EdgeInsets.all(5.0),
+        child: new ListView.builder(
+          itemExtent: 70.0,
+          itemCount: 20,
+          itemBuilder: (_, index) => _buildTableRow(context))
+    );
+  }
+  
+  Widget _buildTableRow(BuildContext context) {
+    return new Container(
+      child: new Row(
+        children: <Widget>[
+          new Expanded(child: _buildTable(context)),
+          new Expanded(child: _buildTable(context)),
+          new Expanded(child: _buildTable(context)),
+        ],
+      ),
+    );
+  }
+  
+  Widget _buildTable(BuildContext context) {
+    return new Container(
+      padding: EdgeInsets.zero,
+      margin: EdgeInsets.zero,
+      child: new Card(
+        color: primaryColor,
+        child: new Row(
+          children: <Widget>[
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: new Icon(Icons.people, size: 16.0,),
+            ),
+            new Expanded(child: new Container()),
+            Padding(
+              padding: const EdgeInsets.all(20.0),
+              child: new Text(
+                '01',
+                style: const TextStyle(
+                    color: fontColor, fontFamily: 'Dosis', fontSize: 20.0
+                ),
+              ),
+            )
+          ],
+        ),
+      )
+    );
   }
 }
