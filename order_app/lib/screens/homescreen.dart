35bc0bacaa0d7f6339c8e88f9e4aeaa0ee8e6f7a
ndc07 <ndc07.it@gmail.com>
2018-10-01 21:42:09 +0700

    menuscreen: design search + filter controls

--- order_app/lib/screens/homescreen.dart
+++ order_app/lib/screens/homescreen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import './../UI/theme.dart';
 import './menuscreen.dart';
+import './cartscreen.dart';
 
 class HomeScreen extends StatefulWidget {
   @override
@@ -11,17 +12,14 @@ class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
     return Container(
-        decoration: new BoxDecoration(
-            borderRadius: BorderRadius.circular(5.0),
-            border: Border.all(color: fontColorLight.withOpacity(0.2))),
-        margin: EdgeInsets.all(5.0),
-        child: new ListView.builder(
+      margin: EdgeInsets.all(5.0),
+      child: new ListView.builder(
           itemExtent: 100.0,
           itemCount: 20,
           itemBuilder: (_, index) => _buildTableRow(context)),
     );
   }
-  
+
   Widget _buildTableRow(BuildContext context) {
     return GestureDetector(
       onTap: () {
@@ -43,42 +41,67 @@ class _HomeScreenState extends State<HomeScreen> {
     Navigator.of(context).push(
       new MaterialPageRoute(builder: (context) {
         return new Scaffold(
-            appBar: new AppBar(
-              title: new Text('Select Foods',style: new TextStyle(color: accentColor, fontFamily: 'Dosis'),),
-              iconTheme: new IconThemeData(color: accentColor),
-              centerTitle: true,
-            ),
-            body: new MenuScreen(),
+          appBar: new AppBar(
+            title: new Text('Select Foods',
+              style: new TextStyle(color: accentColor, fontFamily: 'Dosis'),),
+            iconTheme: new IconThemeData(color: accentColor),
+            centerTitle: true,
+          ),
+          body: new MenuScreen(),
+          floatingActionButton: new FloatingActionButton(
+            onPressed: () {
+              _pushCartScreen();
+            },
+            child: new Icon(Icons.add_shopping_cart),
+            tooltip: 'Add To Cart',
+            backgroundColor: fontColor,
+          ),
         );
       }),
     );
   }
-  
+
+  void _pushCartScreen() {
+    Navigator.of(context).push(
+      new MaterialPageRoute(builder: (context) {
+        return new Scaffold(
+          appBar: new AppBar(
+            title: new Text('Cart',
+              style: new TextStyle(color: accentColor, fontFamily: 'Dosis'),),
+            iconTheme: new IconThemeData(color: accentColor),
+            centerTitle: true,
+          ),
+          body: new CartScreen(),
+        );
+      }),
+    );
+  }
+
   Widget _buildTable(BuildContext context) {
     return new Container(
-      padding: EdgeInsets.zero,
-      margin: EdgeInsets.zero,
-      child: new Card(
-        color: primaryColor,
-        child: new Row(
-          children: <Widget>[
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: new Icon(Icons.people, size: 20.0,),
-            ),
-            new Expanded(child: new Container()),
-            Padding(
-              padding: const EdgeInsets.all(20.0),
-              child: new Text(
-                '01',
-                style: const TextStyle(
-                    color: fontColor, fontFamily: 'Dosis', fontSize: 20.0
-                ),
+        padding: EdgeInsets.zero,
+        margin: EdgeInsets.zero,
+        child: new Card(
+          color: primaryColor,
+          child: new Row(
+            children: <Widget>[
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: new Icon(Icons.people, size: 20.0,),
               ),
-            )
-          ],
-        ),
-      )
+              new Expanded(child: new Container()),
+              Padding(
+                padding: const EdgeInsets.all(20.0),
+                child: new Text(
+                  '01',
+                  style: const TextStyle(
+                      color: fontColor, fontFamily: 'Dosis', fontSize: 20.0
+                  ),
+                ),
+              )
+            ],
+          ),
+        )
     );
   }
 }
