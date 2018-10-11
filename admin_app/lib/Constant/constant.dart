a5c8bf1a2a69bce9fc40ee224f045fd8b12dcce8
Thienlan <meomeocf98@gmail.com>
2018-10-11 22:04:58 +0700

    add Model + MySqlConnection

---
+++ admin_app/lib/Constant/constant.dart
@@ -0,0 +1,4 @@
+const String URL_EXECUTE = "https://meomeocf98.000webhostapp.com/flutter";
+const String url = "https://meomeocf98.000webhostapp.com/flutter/LoadChat.php";
+const String ID_EXECUTENONEQUERY = 'executeNoneQuery';
+const String ID_EXECUTEQUERY = 'executeQuery';
---
+++ admin_app/lib/Mysql/ConnectionMySql.dart
@@ -0,0 +1,72 @@
+//library mysqlconnection;
+import 'package:http/http.dart' as http;
+import '../Constant/constant.dart';
+import 'dart:async';
+import 'dart:convert';
+
+class MySqlConnection{
+
+  static MySqlConnection _instance;
+
+  static MySqlConnection get instance {
+    if(_instance == null) _instance = new MySqlConnection();
+    return _instance;
+  }
+
+  Future<bool> executeNoneQuery(String query, {List parameter}) async {
+    /**
+     * executeNoneQuery without or with parameter
+     * return true or false
+     */
+    if(parameter != null)
+      query = _addParameter(query,  parameter);
+    http.Response response = await http.post(
+      URL_EXECUTE,
+      body : { ID_EXECUTENONEQUERY : query },
+    );
+    int number = int.parse(response.body);
+    return number > 0;
+  }
+
+  Future<List> executeQuery(String query, {List parameter}) async {
+    /**
+     * executeNoneQuery without or with parameter
+     * return List Object
+     */
+    if(parameter != null)
+      query = _addParameter(query,  parameter);
+    http.Response response = await http.post(
+      URL_EXECUTE,
+      body : { ID_EXECUTENONEQUERY : query },
+    );
+    List listObject = json.decode(response.body);
+    return listObject;
+  }
+
+  static String _addParameter(String query, List parameter) {
+    /**To return a query have added paramete ralready
+     * 
+     * query = "call USP_Proc( @a , @b , @c )"
+     * 
+     *  parameter = ["123" , "123" , 123 ]
+     * 
+     *  After call  addParameter
+     * so result query = "call USP_Proc( '123' , '123' , 123 )"
+     * */
+    List<String> list = query.split(' ');
+    query = "";
+    int i = 0;
+    list.forEach((String element) {
+      if (element.contains('@')){
+        if(parameter[i] is String)
+          query +=  "\'" + parameter[i++] +"\'";
+        else query += parameter[i++].toString();
+      }
+        else query += element;
+      query += " ";
+    });
+    return query;
+  }
+
+}
+
