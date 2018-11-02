d84f26e1079ebda9c7582ec5c12ffa9b5bd4e0f7
ndc07 <ndc07.it@gmail.com>
2018-11-02 20:46:21 +0700

    fix errors: "DropdownMenuItem value == null"

--- order_app/lib/Controllers/menu.controller.dart
+++ order_app/lib/Controllers/menu.controller.dart
@@ -11,4 +11,18 @@ class Controller {
   Future<List<FoodCategory>> get foodCategories => Model.instance.foodCategories;
   Future<List<Food>> get foods => Model.instance.foods;
 
+  Future<List<Food>> filterFoods(String category) async {
+    List<Food> _foods = await foods;
+    int idCategory = await getIdCategory(category);
+    if (idCategory == -1) return _foods;
+    return _foods.where((_food) => _food.idFoodCategory == idCategory);
+  }
+
+  Future<int> getIdCategory(String category) async {
+    List<FoodCategory> _foodCategories = await foodCategories;
+    for (var item in _foodCategories) {
+      if (item.name == category) return item.id;
+    }
+    return -1;
+  }
 }
