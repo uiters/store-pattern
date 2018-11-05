150431666b678bc6741e87c3915616b645aeb847
ndc07 <ndc07.it@gmail.com>
2018-11-05 23:13:11 +0700

    fix status table

--- order_app/lib/Models/home.model.dart
+++ order_app/lib/Models/home.model.dart
@@ -55,6 +55,7 @@ class Table {
 
   List<menu.Food> combineFoods(List<menu.Food> _menuFoods) {
     List<menu.Food> menuFoods = List<menu.Food>.from(_menuFoods);
+
     if (foods != null)
     for (var i = 0; i < foods.length; i++) {
       for (var j = 0; j < menuFoods.length; j++) {
@@ -64,13 +65,8 @@ class Table {
         }
       }
     }
-    return menuFoods;
-  }
 
-  void clearMenuFoods(List<menu.Food> menuFoods) {
-    for (var i = 0; i < menuFoods.length; i++) {
-      menuFoods[i].quantity = 0;
-    }
+    return menuFoods;
   }
 
   void addFood(menu.Food food) {
@@ -85,6 +81,10 @@ class Table {
     } else { // update food
       foods[index].quantity++;
     }
+
+    if (foods.length > 0) {
+      this.status = 1;
+    }
   }
 
   void subFood(menu.Food food) {
@@ -95,6 +95,10 @@ class Table {
       } else {
         deleteFood(foods[index]);
       }
+    }
+
+    if (foods.length == 0) {
+      this.status = -1;
     }
   }
 
