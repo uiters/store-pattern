2d43ffb854ab30fd2c88ee392bcda85f760a842b
yeutham212 <meomeocf98@gmail.com>
2018-11-23 02:02:16 +0700

    remove server & database

--- server/index.php
+++
@@ -1,8 +0,0 @@
-<?php
-require_once 'Model/Login.php';
-$header = isset($_SERVER['HTTP_HEADER']) ? $_SERVER['HTTP_HEADER'] : null;
-$body = isset($_POST['header']) ? $_POST['header'] : null;
-//echo "$header<p>$body";
-$login = new Login();
-$login->builder($header, $body);
-?>
